eee25c43ccc03e66d987a0b2b03a7e2ace02a764
0b1afk0 <maravbro@gmail.com>
2025-02-24 01:34:27 +0400

    die commit
    Добавлена анимация и звук смерти игрока(Чуть позже поищу способ добавить переход на сцену финиша.)

--- bu-ispugalsa-main/Assets/Scripts/EnemyAI.cs
+++ bu-ispugalsa-main/Assets/Scripts/EnemyAI.cs
@@ -1,16 +1,19 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.SceneManagement;
 
 public class EnemyAI : MonoBehaviour
 {
-    public Transform player;  // Ссылка на игрока
+    public Transform player;
     public NavMeshAgent agent;
-    public Animator monsterAnimator;
+    public Animator walkAnimator;        // Аниматор модели, которая ходит
+    public GameObject normalModel;       // Модель, которая ходит
+    public GameObject killerModel;       // Модель, которая бьёт
+    public Animator killerAnimator;      // Аниматор модели, которая бьёт
 
     private bool isAttacking = false;
+    private bool isDead = false;
 
     private void Start()
     {
@@ -40,26 +43,46 @@ public class EnemyAI : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !isAttacking)
         {
-            StartCoroutine(AttackPlayer(other));
+            StartCoroutine(AttackPlayer(other.GetComponent<PlayerDeath>()));
         }
     }
 
-    private IEnumerator AttackPlayer(Collider player)
+    private IEnumerator AttackPlayer(PlayerDeath playerDeath)
     {
         isAttacking = true;
-        agent.isStopped = true; // Останавливаем монстра
-        monsterAnimator.SetBool("isAttacking", true);
+        agent.isStopped = true;
+        walkAnimator.enabled = false;
 
-        yield return new WaitForSeconds(6f); // Ждём завершения анимации удара (настрой по длительности анимации)
+        // Перемещаем KillerModel на позицию BooWalking
+        killerModel.transform.SetPositionAndRotation(normalModel.transform.position, normalModel.transform.rotation);
 
-        // Здесь можно добавить логику смерти игрока
-        Debug.Log("Игрок убит!");
-        SceneManager.LoadScene("GameOver");
+        // Включаем модель убийства и отключаем ходьбу
+        normalModel.SetActive(false);
+        killerModel.SetActive(true);
 
-        monsterAnimator.SetBool("isAttacking", false);
-        agent.isStopped = false;
-        isAttacking = false;
+        // Поворачиваем монстра к игроку
+        Vector3 lookPos = player.position - transform.position;
+        lookPos.y = 0;
+        transform.rotation = Quaternion.LookRotation(lookPos);
+
+        // Привязываем камеру к KillerModel
+        Camera.main.transform.SetParent(killerModel.transform);
+        Camera.main.transform.localPosition = new Vector3(34.4f, 21.3f, 392.2003f); // Координаты камеры
+        Camera.main.transform.localRotation = Quaternion.Euler(-38.705f, -182.277f, 3.839f);
+
+        // Отключаем игрока
+        player.gameObject.SetActive(false);
+
+        // Проигрываем анимацию удара
+        killerAnimator.SetTrigger("Attack");
+
+        // Ждем завершения анимации удара
+        yield return new WaitForSeconds(killerAnimator.GetCurrentAnimatorStateInfo(0).length);
+
+        // После завершения анимации вызовем метод Die() и перейдем на сцену "finish"
+        playerDeath.Die();
     }
+
 }
--- bu-ispugalsa-main/Assets/Scripts/PlayerDeath.cs
+++ bu-ispugalsa-main/Assets/Scripts/PlayerDeath.cs
@@ -1,54 +1,21 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerDeath : MonoBehaviour
 {
-    public Camera playerCamera;
     public Transform monster;
+    public Transform killer;
     public float deathCameraTurnSpeed = 2f;
 
     private bool isDead = false;
 
     public void Die()
     {
+        isDead = true;
         if (isDead) return;
 
-        isDead = true;
-        StartCoroutine(HandleDeath());
-    }
-
-    private IEnumerator HandleDeath()
-    {
-        // Останавливаем движение игрока
-        GetComponent<PlayerMovement>().enabled = false;
-
-        // Плавно поворачиваем камеру к монстру
-        while (true)
-        {
-            Vector3 directionToMonster = (monster.position - playerCamera.transform.position).normalized;
-            Quaternion lookRotation = Quaternion.LookRotation(directionToMonster);
-            playerCamera.transform.rotation = Quaternion.Slerp(playerCamera.transform.rotation, lookRotation, Time.deltaTime * deathCameraTurnSpeed);
-
-            // Проверяем, достаточно ли камера повернулась
-            if (Quaternion.Angle(playerCamera.transform.rotation, lookRotation) < 1f)
-            {
-                break;
-            }
-
-            yield return null;
-        }
-
-        // Проигрываем анимацию атаки монстра
-        Animator monsterAnimator = monster.GetComponent<Animator>();
-        if (monsterAnimator != null)
-        {
-            monsterAnimator.SetTrigger("Attack"); // Убедись, что триггер "Attack" есть в анимациях монстра
-        }
-
-        // Ждем завершения анимации (предположим, она длится 3 секунды)
-        yield return new WaitForSeconds(3f);
-
-        // Здесь можно добавить, например, переход на экран "Game Over"
         Debug.Log("Игрок погиб!");
+        SceneManager.LoadScene("finish");
     }
 }
